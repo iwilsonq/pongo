f93d0e8f5e3a45079b35a0f7d3498b75e7d83fab
Roman Morozov <sublimeye.ua@gmail.com>
2017-12-08 17:34:48 -0800

    chore(codestyle): code formatting

--- main.go
+++ main.go
@@ -1,73 +1,73 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
 )
 
 var userList = []User{
-	User{
-		FirstName: "Roman",
-		LastName:  "Morozov",
-		Email:     "sublimeye@gmail.com",
-	}}
+  User{
+    FirstName: "Roman",
+    LastName:  "Morozov",
+    Email:     "sublimeye@gmail.com",
+  }}
 
 var router *gin.Engine
 
 func initializeRoutes() {
-	router.GET("/users", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Found users.",
-			"items":   userList,
-		})
-	})
+  router.GET("/users", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "Found users.",
+      "items":   userList,
+    })
+  })
 
-	router.POST("/login", func(c *gin.Context) {
-		var user User
-		c.BindJSON(&user)
+  router.POST("/login", func(c *gin.Context) {
+    var user User
+    c.BindJSON(&user)
 
-		if isEmailExists(user.Email) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "login ok",
-			})
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"message": "user not found, please sign up!",
-			})
-		}
-	})
+    if isEmailExists(user.Email) {
+      c.JSON(http.StatusOK, gin.H{
+        "message": "login ok",
+      })
+    } else {
+      c.JSON(http.StatusNotFound, gin.H{
+        "message": "user not found, please sign up!",
+      })
+    }
+  })
 
-	router.POST("/signup", func(c *gin.Context) {
-		var user User
-		c.BindJSON(&user)
+  router.POST("/signup", func(c *gin.Context) {
+    var user User
+    c.BindJSON(&user)
 
-		if isEmailExists(user.Email) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "signup ok",
-			})
-			return
-		}
+    if isEmailExists(user.Email) {
+      c.JSON(http.StatusOK, gin.H{
+        "message": "signup ok",
+      })
+      return
+    }
 
-		if isUserValid(user) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "signup ok",
-			})
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"message": "user not found, please sign up!",
-			})
-		}
-	})
+    if isUserValid(user) {
+      c.JSON(http.StatusOK, gin.H{
+        "message": "signup ok",
+      })
+    } else {
+      c.JSON(http.StatusNotFound, gin.H{
+        "message": "user not found, please sign up!",
+      })
+    }
+  })
 }
 
 func main() {
-	fmt.Println("Starting...")
+  fmt.Println("Starting...")
 
-	router = gin.Default()
+  router = gin.Default()
 
-	initializeRoutes()
+  initializeRoutes()
 
-	router.Run()
+  router.Run()
 }
